12bd5813116f7c7c3fd4de5c6203856271ed3da1
marcesso <marc.esso@postlight.com>
2018-08-06 11:15:06 +0300

    fix: proxy error

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  // Initialize state
   state = { passwords: [] };
-
-  // Fetch passwords after first mount
   componentDidMount() {
     this.getPasswords();
   }
 
   getPasswords = () => {
-    // Get the passwords and store them in state
-    fetch("/api/passwords", {
-      method: "GET"
-    })
+    fetch("/api/passwords")
       .then(res => res.json())
       .then(passwords => this.setState({ passwords }));
   };
@@ -24,17 +18,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        {/* Render the passwords if we have them */}
         {passwords.length ? (
           <div>
             <h1>5 Passwords.</h1>
             <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
               {passwords.map((password, index) => (
                 <li key={index}>{password}</li>
               ))}
@@ -44,7 +31,6 @@ class App extends Component {
             </button>
           </div>
         ) : (
-          // Render a helpful message otherwise
           <div>
             <h1>No passwords :(</h1>
             <button className="more" onClick={this.getPasswords}>
