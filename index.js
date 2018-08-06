6d39de711e92008a35a88fc7741e0b308119a85c
marcesso <marc.esso@postlight.com>
2018-08-06 10:57:32 +0300

    changing url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/api/password", (req, res) => {
+app.get("/api/passwords", (req, res) => {
   const count = 5;
   const passwords = Array.from(Array(count).keys()).map(i =>
     generatePassword(12, false)
@@ -19,7 +19,6 @@ app.get("/api/password", (req, res) => {
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  console.log("here");
 });
 
 const port = process.env.PORT || 5000;
